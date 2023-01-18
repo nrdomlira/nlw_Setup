fe4f42624d5397a55a65830ecd2b92c4fb908ba3
José Domingos <domliraa@gmail.com>
2023-01-18 19:28:20 -0300

    add tables and create routes

---
+++ prisma/migrations/20230118195445_create_rest_of_tables/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "habit_week_days" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "habit_id" TEXT NOT NULL,
+    "week_day" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "days" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "DayHabit" (
+    "id" TEXT NOT NULL PRIMARY KEY
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "habit_week_days_habit_id_week_day_key" ON "habit_week_days"("habit_id", "week_day");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "days_date_key" ON "days"("date");
